cef8566473ddedab694760972241cf7295144c8d
kreetigulati <kreetigulati@outlook.com>
2021-07-19 14:43:38 -0700

    adding level 1 work

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,46 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// table body
+var tbody = d3.select("tbody");
+
+// Columns
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Button 
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    tbody.html("");
+
+    // input date
+    var inputElement = d3.select("#datetime");
+    // date, state, shape - value property
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    // filter data - datetime = inputValue
+    var filterData = tableData.filter(sighting => sighting.datatime === inputValue);
+    console.loge(filterData);
+
+    filterData.forEach(function(selections) {
+        console.log(selections);
+        // append new table row for each object 
+        var row = tbody.append("tr");
+        // console.log each UFO Sighting value 
+        Object.entries(selections).forEach(function([key, value]) {
+            console.log(key, value);
+            // append row for each value
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
