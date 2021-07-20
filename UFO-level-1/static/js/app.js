6121d7e159305aa3ae4c4ab582ea4ff18f1084df
kreetigulati <kreetigulati@outlook.com>
2021-07-19 21:21:18 -0700

    adding homework for submission

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,4 @@
-// from data.js
+// NEW START 
 var tableData = data;
 
 // table body
@@ -16,33 +16,48 @@ tableData.forEach(ufoSighting => {
     });
 });
 
-// Button 
-var button = d3.select("#filter-btn");
-button.on("click", function() {
 
-    tbody.html("");
+// Select the button
+var button = d3.select("#datetime");
 
-    // input date
-    var inputElement = d3.select("#datetime");
-    // date, state, shape - value property
-    var inputValue = inputElement.property("value");
-        console.log(inputValue);
-    // datetime = input value
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-        console.log(filteredData);
+// Select the form
+var form = d3.select("#filter-btn");
 
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
 
-    filteredData.forEach(function(selections) {
+// tbody.html("");
 
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datatime === inputValue);
+
+  console.log(filteredData);
+
+  filteredData.forEach(function(selections) {
     console.log(selections);
-    // append row 
+    // append new table row for each object 
     var row = tbody.append("tr");
-    
+    // console.log each UFO Sighting value 
     Object.entries(selections).forEach(function([key, value]) {
         console.log(key, value);
-        // append cell 
+        // append row for each value
         var cell = row.append("td");
         cell.text(value);
     });
-    });
-});
+  });
+};
